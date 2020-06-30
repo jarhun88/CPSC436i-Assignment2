f46f54ee23ba522ddd9bbbce6513aaafb1fc2436
John <likisongproduct@gmail.com>
2020-06-30 00:10:55 -0700

    working get and delete endpoints

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,12 +1,18 @@
 let messageSchema = require('../models/messageSchema');
 let {v4: uuidv4} = require('uuid');
+const app = require('../app');
 
-const getMessages = (req, res, next) => {
-
-    res.status(200).json({message: 'got messages'});
+const getMessages = async (req, res) => {
+    try {
+        const messages = await messageSchema.find();
+        return res.json(messages);
+    } catch (err) {
+        console.err(err);
+        return res.status(500).send(err);
+    }    
 };
 
-const createMessage = (req, res, next) => {
+const createMessage = (req, res) => {
     const message = new messageSchema({
         _id: uuidv4(),
         text: req.body.text,
@@ -22,8 +28,20 @@ const createMessage = (req, res, next) => {
     
 };
 
-const deleteMessage = (req, res, next) => {
-    res.status(200).json({message: 'message deleted'});
+const deleteMessage = async (req, res) => {
+    try {
+        console.log(req.body._id)
+        const messageDelete = await messageSchema.deleteOne({_id: req.body._id});
+        console.log(messageDelete);
+        if (messageDelete) {
+            res.status(200).json({message: 'successfully deleted'});
+        } else {
+            res.status(500).json({message: 'could not delete'});
+        }
+    } catch (err) {
+        // console.err(err);
+        return res.status(500).send(err);
+    }
 };
 
 module.exports = {getMessages, createMessage, deleteMessage};
